174415bf0ff736d100f71e558e379052bfa15026
Mateusz Chochol <mch.priv@gmail.com>
2019-02-16 01:30:11 +0100

    Upload all

---
+++ Assets/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public QuestionDefinition[] questions;
+    public Text questionText;
+    public GameObject restartButton;
+    private int points = 0;
+    private int questionIndex = 0;
+
+    private void Start() {
+        restartButton.SetActive(false);
+        LoadQuestion();
+    }
+
+    private void LoadQuestion() {
+        if (questionIndex < questions.Length) {
+            questionText.text = questions[questionIndex].question;
+        }
+        else {
+            EndQuiz();
+        }
+        Debug.Log(points);
+    }
+
+    private void EndQuiz() {
+        restartButton.SetActive(true);
+        questionText.text = "Koniec quizu. Poprawne odpowiedzi: " + points + "/" + questionIndex;
+    }
+
+    public void TrueAnswer() {
+        if (questionIndex < questions.Length) {
+            if (questions[questionIndex].trueOrFalse == true) {
+                Debug.Log("Point added");
+                points++;
+            }
+            else {
+                Debug.Log("Wrong answer");
+            }
+            Debug.Log("Next question loaded");
+            questionIndex++;
+            LoadQuestion();
+        }
+    }
+
+    public void FalseAnswer() {
+        if (questionIndex < questions.Length) {
+            if (questions[questionIndex].trueOrFalse == false) {
+                Debug.Log("Point added");
+                points++;
+            }
+            else {
+                Debug.Log("Wrong answer");
+            }
+            Debug.Log("Next question loaded");
+            questionIndex++;
+            LoadQuestion();
+        }
+    }
+
+    public void RestartTheGame() {
+        questionIndex = 0;
+        points = 0;
+        restartButton.SetActive(false);
+        LoadQuestion();
+    }
+}
---
+++ Assets/QuestionDefinition.cs
@@ -0,0 +1,5 @@
+﻿[System.Serializable]
+public class QuestionDefinition : System.Object {
+    public string question;
+    public bool trueOrFalse;
+}
